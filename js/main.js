a1cc2cd07d01db0619adcc5809be712c88885512
theunclehonnor <jekbormoley@gmail.com>
2019-08-30 01:09:53 +0300

    Сделали хедер. без адптива

--- js/main.js
+++ js/main.js
@@ -1,6 +1,17 @@
-// Анимация медленной прокрутки
-$("a[href^='#']").click(function () {
-	var _href = $(this).attr("href");
-	$("html, body").animate({ scrollTop: $(_href).offset().top - 120 + "px" });
-	return false;
+//On_load
+$(window).load(function () {
+	// Slider slick
+	$('.home-slider').slick({
+		draggable: false,
+		dots: true,
+		dotsClass: 'home-slider__dots',
+		prevArrow: $('.arrow-left'),
+		nextArrow: $('.arrow-right')
+	});
+	// Анимация медленной прокрутки
+	$("a[href^='#']").click(function () {
+		var _href = $(this).attr("href");
+		$("html, body").animate({ scrollTop: $(_href).offset().top - 120 + "px" });
+		return false;
+	});
 });
